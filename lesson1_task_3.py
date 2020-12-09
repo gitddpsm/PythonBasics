1bf3978ee80abd112bd1303595969c8595bbe568
gitddpsm <37410949+gitddpsm@users.noreply.github.com>
2020-12-09 19:55:24 +0300

    Update lesson1_task_3.py
    🤯

--- lesson1_task_3.py
+++ lesson1_task_3.py
@@ -12,10 +12,16 @@ def input_integer():
             print('Error! signature you have typed is not a number!')
 
 count = 0
-user_input = input_integer()
+tmp = user_input = 3 # input_integer()
+
+while tmp:  # Пока tmp True, то есть больше нуля
+    # tmp = tmp // 10
+    # Как только при отделении остатка от числа tmp доходит до 0 оно выдаёт False и цикл прекращается (🤯)
+    tmp //= 10
+    count += 1
 
 nn_div = 10 ** count + 1
 nnn_div = (10 ** (count * 2)) + nn_div
-result = user_input + (user_input * nn_div) + (user_input * nnn_div)
 
+result = user_input + (user_input * nn_div) + (user_input * nnn_div)
 print(f"Cумма чисел n + nn + nnn  = {result}")
