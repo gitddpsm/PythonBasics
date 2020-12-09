8071f47f56a57dd6dfa8aeddc00730921fd7a79e
gitddpsm <37410949+gitddpsm@users.noreply.github.com>
2020-12-09 18:19:08 +0300

    Create lesson1_task_3.py
    Задание 3

---
+++ lesson_1/lesson1_task_3.py
@@ -0,0 +1,21 @@
+'''3. Узнайте у пользователя число n. Найдите сумму чисел n + nn + nnn.'''
+
+def input_integer():
+    # Проверяет ввод, возвращает введенное число 
+    # Дает возможность повторить ввод при ошибке
+    while True:
+        _input = input('\nEnter a number:\n')   
+        if _input.isdigit():
+            _input = int(_input)
+            return _input
+        else:
+            print('Error! signature you have typed is not a number!')
+
+count = 0
+tmp = user_input = input_integer()
+
+nn_div = 10 ** count + 1
+nnn_div = (10 ** (count * 2)) + nn_div
+result = user_input + (user_input * nn_div) + (user_input * nnn_div)
+
+print(f"Cумма чисел n + nn + nnn  = {result}")
