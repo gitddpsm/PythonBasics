27ac3abf251d3051d34aeb23a8c7d62b12382eb6
gitddpsm <37410949+gitddpsm@users.noreply.github.com>
2020-12-23 12:33:27 +0300

    upd task6
    upd task6

--- les3_task6.py
+++ les3_task6.py
@@ -1,4 +1,16 @@
 '''6) Реализовать функцию int_func(), принимающую слово из маленьких латинских букв и возвращающую его же, но с прописной первой буквой. Например, print(int_func(‘text’)) -> Text. 
 Продолжить работу над заданием. В программу должна попадать строка из слов, разделенных пробелом. Каждое слово состоит из латинских букв в нижнем регистре. Сделать вывод исходной строки, но каждое слово должно начинаться с заглавной буквы. Необходимо использовать написанную ранее функцию int_func().'''
 
-input('ok _>')
+def int_func(word):
+    return word.title()
+ 
+def mass_titles(words_string):
+    tmp_list = words_string.split()
+    out_str = ''
+    for el in tmp_list:
+        out_str = (f'{out_str} {int_func(el)}')
+    return out_str
+
+print(mass_titles('sdjfh sdkjhfkj dskjhf eqwrio qwp'))    
+
+input('ok >')
