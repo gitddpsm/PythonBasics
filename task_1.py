131a21d2eed27946e6e33a8e878d98f42f327797
Don Diamond <ddpsm@ya.ru>
2020-12-27 01:48:22 +0300

    task 1
    как сделать лямбда функцией ?

--- task_1.py
+++ task_1.py
@@ -1,4 +1,26 @@
-'''1. Реализовать скрипт, в котором должна быть предусмотрена функция расчета заработной платы сотрудника. В расчете необходимо использовать формулу: (выработка в часах * ставка в час) + премия. Для выполнения расчета для конкретных значений необходимо запускать скрипт с параметрами.'''
+from sys import argv
 
+if len(argv) < 4:
+    print('error attributes less 3 digits')
+    exit()
 
-(выработка в часах * ставка в час) + премия.
+values = []
+
+for el in argv[1:]:
+    try:
+        el = int(el)
+        values.append(el)
+    except ValueError:
+        print('you need use 3 digits\n[выработка в часах] [ставка в час] [премия]')
+        exit() 
+
+#values = [20, 300, 2050]
+#print(lambda work_time, mp_hour, prem: (work_time * mp_hour) + prem, values)
+
+def out_final(*args):
+    params = args[0]
+    out = params[0] * params[1] + params[2]
+    return out 
+
+print(out_final(values))
+exit()
